8f30f8290e0c4b15aae6fde7095926c087b0eff4
pe222pe <g1744609@cc.kyoto-su.ac.jp>
2019-10-15 14:07:54 +0900

    bootstrap修正中

--- guide/select_date.php
+++ guide/select_date.php
@@ -4,11 +4,18 @@
   <meta charset="UTF-8">
   <title>変更・追加・削除希望の日選択</title>
 
-  <link rel="stylesheet" href="../css/0-3-A1.css">
-  <link rel="stylesheet" href="../css/0-3-A3.css">
+  <!--<link rel="stylesheet" href="../css/0-3-A1.css">
+  <link rel="stylesheet" href="../css/0-3-A3.css">-->
   <link rel="stylesheet" href="../css/calender.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"\
+	  crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" cr\
+	  ossorigin="anonymous"></script>
 
 <?php
  $area = $_POST['area'];
@@ -20,18 +27,25 @@
 </head>
 <body margin:auto; text-align:center;>
 
- <div class="main">
-    <div class="startup">
-      <?php if(isset($_GET["id"])) echo $gid = $_GET['id'];else echo $gid = $_POST['gid']; ?>さんのガイド可能日と予約日の確認中
-   </div>
- <div class="message">
-        カレンダーの日を
-        クリックして下さい。
- </div>
+  <div class="main">
+    <div class="container-fluid">
+      <div class="text-center">
+	<div class="startup">
+	  <?php if(isset($_GET["id"])) echo $gid = $_GET['id'];else echo $gid = $_POST['gid']; ?>さんのガイド可能日と予約日の確認中
+	</div>
+	<div class="message">
+          カレンダーの日を
+          クリックして下さい。
+	</div>
  ※緑はガイド可能な日として登録されている日です
 <!-- 
  <br> ※赤はガイド予定（予約）が入っている日です
---> 
+ -->
+      
+      </div>
+    </div>
+    
+    
 </div>
 
    <form name="myform3" method='post' onsubmit="return checkText3()">
@@ -324,24 +338,37 @@ list($row_month, $row_day)=explode("-", $row['date']);
 <input name = "month" type="hidden" >
 <input name = "day" type="hidden" >
 
- <div class="main">
-        <div class="select-btn guide-select">
-          <select name="mode" require>
-            <option value="1" >登録(追加)</option>
-            <option value="0" >変更</option>
-            <option value="2" >削除</option>
-          </select>
-        </div>
-        <p></p>
+<div class="main">
+  <div class="container-fluid">
+    <div class="text-center">
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+          <div class="form-group">
+            <div class="select-btn guide-select">
+              <select name="mode" require class="form-control">
+		<option value="1" >登録(追加)</option>
+		<option value="0" >変更</option>
+		<option value="2" >削除</option>
+              </select>
+            </div>
+	  </div>
+	</div>
       </div>
+      <p></p>
 
-        <p></p>
-
-       <input id="submit_btn" class = "btn resistration" type="submit" value="Next">
-        <p></p>
+      <p></p>
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+	  <input id="submit_btn" class = "btn btn-info btn-lg btn-block" type="submit" value="Next">
+	</div>
       </div>
- </div>
-
+      
+      <p></p>
+    </div>
+  </div>
+</div>
 </form>
- </body>
+</body>
 </html>
