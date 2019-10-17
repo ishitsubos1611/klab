1c60ce5e149c444c7e95426891097472a5522be6
pe222pe <g1744609@cc.kyoto-su.ac.jp>
2019-10-17 14:55:26 +0900

    bootstrap修正

--- guide/select_area4booking.php
+++ guide/select_area4booking.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <title>STEP 1</title>
 
-  <link rel="stylesheet" href="../css/0-3-A1.css">
+  <!-- <link rel="stylesheet" href="../css/0-3-A1.css"> -->
+  <link rel="stylesheet" href="../css/bootstrap.css">
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 </head>
@@ -21,39 +22,64 @@
 
 ?>
 <form name="step1" action="select_date4booking.php" method='post'>
- <div class="main">
-      <div class="btn-wrapper">
+  <div class="main">
+    <div class="container-fluid">
+      <div class="text-center">
+	<div class="btn-wrapper">
+	  <br><br>
 
+          <p>IDを入力して下さい</p>
+	  
+          <div class="select-btn guide-select">
+            <input name = "gid" style=text value="1">
+          </div>
+	
+	  <p></p>
+          <p>エリアを選択してください</p>
+	  <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+              <div class="form-group">
+		<div class="select-btn guide-select">
+		  <select name="area" class="form-control">
+		    <option value="kyoto" >近畿（京都）</option>
+		    <option value="kanto" >関東</option>
+		  </select>
+		</div>
+	      </div>
+	    </div>
+          </div>
 
-        <p>IDを入力して下さい</p>
-        <div class="select-btn guide-select">
-          <input name = "gid" style=text value="1">
-        </div>
+          <p>名所か道案内か選択してください</p>
+	  <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+              <div class="form-group">
+		<div class="select-btn guide-select">
+		  <select name="style" class="form-control">
+		    <option value="1" >名所ガイド</option>
+		    <option value="2" >道案内ガイド</option>
+		  </select>
+		</div>
+	      </div>
+	    </div>
+          </div>
 
-        <p>エリアを選択してください</p>
-        <div class="select-btn guide-select">
-          <select name="area" >
-            <option value="kyoto" >近畿（京都）</option>
-            <option value="kanto" >関東</option>
-          </select>
-        </div>
-
-        <p>名所か道案内か選択してください</p>
-        <div class="select-btn guide-select">
-          <select name="style" >
-            <option value="1" >名所ガイド</option>
-            <option value="2" >道案内ガイド</option>
-          </select>
-        </div>
-
-        <p></p>
-
-       <input class = "btn resistration" type="submit" value="Next">
-        <p></p>
+          <p></p>
+	  <div class="row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+	      <input class = "btn btn-info btn-lg btn-block" type="submit" value="Next">
+	    </div>
+	  </div>
+	 
+          <p></p>
+	</div>
       </div>
- </div>
- </form> 
+    </div>
+  </div>
+</form> 
 
 
- </body>
+</body>
 </html>
