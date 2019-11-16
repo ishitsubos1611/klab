c16d2f46133888c16df1440da61d4cd7e8c82734
pe222pe <g1744609@cc.kyoto-su.ac.jp>
2019-11-16 15:24:53 +0900

    ナビゲーションバー

--- user/user_select_date.php
+++ user/user_select_date.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <title>変更・追加・削除希望の日選択</title>
 
-  <link rel="stylesheet" href="../css/0-3-A1.css">
-  <link rel="stylesheet" href="../css/0-3-A3.css">
+  
+  <link rel="stylesheet" href="../css/bootstrap.css">
   <link rel="stylesheet" href="../css/calender.css">
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 <?php
  $area = $_POST['area'];
@@ -23,20 +24,70 @@
 </head>
 <body margin:auto; text-align:center;>
 
- <div class="main">
-    <div class="startup">
-      <?php if(isset($_GET["uid"])) echo $gid = $_GET['uid'];else echo $uid = $_POST['uid']; ?>さんのガイド希望日を選択中
-   </div>
- <div class="message">
+  <div class="main">
+    <nav class="navbar navbar-dark bg-dark fixed-top">			
+      <a class="navbar-brand" href="../top.html">シェアリングツアーガイド</a>				
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+      </button>		
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+	<ul class="navbar-nav mr-auto">
+	    <!--<li class="nav-item active">
+	      <a class="nav-link" href="#"><span class="sr-only">(カレント)</span></a>
+	    </li>-->
+	    <!--<li class="nav-item">
+	      <a class="nav-link" href="#">ガイドログイン</a>
+	    </li>
+	    <li class="nav-item active">
+	      <a class="nav-link" href="#">ガイド登録</a>
+	    </li>
+	    <li class="nav-item">
+              <a class="nav-link" href="select_area.php">ガイド日程登録</a>
+            </li>
+	    <li class="nav-item">
+              <a class="nav-link" href="select_area4booking.php">ガイド予約確認</a>
+            </li>
+	    <div class="dropdown-divider"></div>
+	    <li class="nav-item">
+              <a class="nav-link" href="#">ユーザログイン</a>
+            </li>  -->
+	    <li class="nav-item">
+              <a class="nav-link" href="../user/user_select_area.php">ユーザ希望登録</a>
+            </li>
+	    <!--<div class="dropdown-divider"></div>-->
+	    <li class="nav-item">
+              <a class="nav-link" href="../realtime/realtime_place_registration.php">今すぐ登録</a>
+            </li>
+	  </ul>
+	</div>
+      </nav>
+
+    <div class="container-fluid">
+      <div class="text-center">
+	<br><br><br>
+        <!--<div class="row">
+          <div class="col-sm-2"></div>
+          <div class="col-sm-8">  -->
+	    <!--<div class="startup">-->
+	    <a href="#"  class = "btn btn-danger disabled btn-xl btn-block">
+              <?php if(isset($_GET["uid"])) echo $gid = $_GET['uid'];else echo $uid = $_POST['uid']; ?>さんのガイド希望日を選択中
+	    </a>
+      <!--</div>-->
+       <!--   </div>
+	</div>   -->
+	
+	<div class="message">
         カレンダーの日を
         クリックして下さい。
- </div>
- ※緑は既に登録されている日です
+	</div>
+	<p class="h4">※<span class="text-success">緑</span>は既に登録されている日です</p>
 <!-- 
  <br> ※赤はガイド予定（予約）が入っている日です
 --> 
-</div>
-
+      </div>
+    </div>
+  </div>
+  
    <form name="myform3" method='post' onsubmit="return checkText3()">
 
 <?php 
@@ -261,7 +312,7 @@ console.log('<?php echo $language ?>');
       $('#cal').show();
       $('#next').show();
       
-      $('#clickedDate').html('<div class="info">選択したガイド希望日：'+yearVal+'年'+monthVal+'月'+dayVal+'日</div>');
+      $('#clickedDate').html('<div class="info text-center h4">選択したガイド希望日：'+yearVal+'年'+monthVal+'月'+dayVal+'日</div>');
 
       if (cellnum == previousNum){
           $('#cal').hide();
@@ -274,9 +325,10 @@ console.log('<?php echo $language ?>');
           //etimeVal = data[i].end_time;
           //console.log(data[i].date);  
           $('#cal').html(
-          '<p class = "select-title">ガイド開始時間</p>'
-          +'<div class="select-btn guide-select"><select name=start_time><option value="08:00:00">8:00</option><option value="09:00:00">9:00</option><option value="10:00:00">10:00</option><option value="11:00:00">11:00</option><option value="12:00:00">12:00</option><option value="13:00:00">13:00</option><option value="14:00:00">14:00</option><option value="15:00:00">15:00</option><option value="16:00:00">16:00</option><option value="17:00:00">17:00</option></select></div>');
-      $('#next').html('<p></p>'+'<input name="yearVal" type="hidden" value="'+yearVal+'">'+'<input name="date" type="hidden" value="'+cellnum+'">'+'<input id="submit_btn" class = "btn resistration" type="submit" value="ガイドさんの検索へ">');
+          '<br><div class="row"><div class="col-sm-6"><div class="select-title text-center">ガイド開始時間</div></div>'
+          +'<div class="col-sm-6"><div class="form-group"><div class="select-btn guide-select"><select name="start_time" class="form-control"><option value="08:00:00">8:00</option><option value="09:00:00">9:00</option><option value="10:00:00">10:00</option><option value="11:00:00">11:00</option><option value="12:00:00">12:00</option><option value="13:00:00">13:00</option><option value="14:00:00">14:00</option><option value="15:00:00">15:00</option><option value="16:00:00">16:00</option><option value="17:00:00">17:00</option></select></div></div></div></div>');
+$('#next').html('<br>'+'<input name="yearVal" type="hidden" value="'+yearVal+'">'+'<input name="date" type="hidden" value="'+cellnum+'">'
++'<input id="submit_btn" class="btn btn-outline-info btn-lg btn-block" type="submit" value="ガイドさんの検索へ">');
         }
      
       for(var i in data){
@@ -287,7 +339,7 @@ console.log('<?php echo $language ?>');
            etimeVal = data[i].end_time.split(":");
            etime_h = parseInt(etimeVal[0],10);
            etime_m = parseInt(etimeVal[1],10);
-           $('#cal').prepend('<p>　(人気)　'+data[i].location+'ガイド </p>'+'<table id="states"><tr><td>Date</td><td>'+data[i].date+'</td></tr><tr><td>UID</td><td>'+data[i].UID+'</td></tr><tr><td>Boarding time</td><td>'+data[i].start_time+'</td></tr>'+'<tr><td>END</td><td>'+data[i].end_time+'</td><td>('+data[i].period+'min)</td></tr>'+'<tr><td>Language</td><td>'+data[i].language+'</td></tr>'+'<tr><td>Total fee</td><td>¥'+data[i].charge+'</td><td>('+data[i].max_num_participant+'名)</td></tr></table>');
+           $('#cal').prepend('<div id="guide" class="info text-center h4"><p>　(人気)　'+data[i].location+'ガイド </p>'+'<table id="states"><tr><td>Date</td><td>'+data[i].date+'</td></tr><tr><td>UID</td><td>'+data[i].UID+'</td></tr><tr><td>Boarding time</td><td>'+data[i].start_time+'</td></tr>'+'<tr><td>END</td><td>'+data[i].end_time+'</td><td>('+data[i].period+'min)</td></tr>'+'<tr><td>Language</td><td>'+data[i].language+'</td></tr>'+'<tr><td>Total fee</td><td>¥'+data[i].charge+'</td><td>('+data[i].max_num_participant+'名)</td></tr></table></div>');
            break;
          }
        }
